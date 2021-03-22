0618a1f954bc2c9085e0c12bc696bbaf6f267aa6
zanderson1998 <zanderson1998@yahoo.com>
2021-03-22 15:16:55 -0600

    add framework for routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,16 @@
+// /api/thoughts
+//get all thoughts
+
+//get one thought
+
+//post new thought
+
+// /api/thoughts/:thoughtid
+//update a thought
+
+//delete thought
+
+// /api/thoughts/:thoughtId/reactions
+//post single reaction
+
+//delete reaction
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
 //import functions
 
-//routes
+// /api/users
+//get all
+
+//get one 
+
+//post new user
+
+// /api/users/:id
+//update a user
+
+//delete a user
+//bonus <- remove users associated thoughts on delete
+
+
+// /api/users/:id/friends/:friendid
+//post new friend to user friend list
+
+//delete a friend from user friend list
 
 
 module.exports = router;

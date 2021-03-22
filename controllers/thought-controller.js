da4965f7bde7e8c92396ef532356db37e7d4a854
zanderson1998 <zanderson1998@yahoo.com>
2021-03-22 16:46:09 -0600

    add most thought routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,13 +1,53 @@
 const { User, Thought } = require('../models/');
 
-// const thoughtController = {
-//     getAllThoughts(){},
-//     getThoughtById(){},
-//     createThought(){},
-//     updateThought(){},
-//     deleteThought(){},
-//     postReaction(){},
-//     deleteReaction(){}
-// }
+const thoughtController = {
+    //get all thoughts
+    getAllThoughts(req,res){
+        Thought.find({})
+        .select('-__v')
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .sort({_id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
+    //get a single thought
+    getThoughtById({params},res){
+        Thought.findOne({ _id: params.id})
+        .select('-__v')
+        .populate({
+            path: 'reactions',
+            select: '-__v'
+        })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    //create a thought
+    createThought({body},res){
+        Thought.create(body)
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.status(400).json(err));
+    },
+    //update a thought
+    updateThought(){},
+    //delete a thought
+    deleteThought(){},
+    // postReaction(){},
+    // deleteReaction(){}
+}
 
 module.exports = thoughtController;
--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,26 +1,26 @@
 const router = require('express').Router();
 
-// //import all functions
-// const {
-//     getAllThoughts,
-//     getThoughtById,
-//     createThought,
-//     updateThought,
-//     deleteThought,
-//     postReaction,
-//     deleteReaction
-// } = require('../../controllers/thought-controller');
+//import all functions
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    postReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
 
-// // /api/thoughts
-// router.route('/')
-// .get(getAllThoughts)
-// .post(createThought);
+// /api/thoughts
+router.route('/')
+.get(getAllThoughts)
+.post(createThought);
 
-// // /api/thoughts/:id
-// router.route('/:id')
-// .get(getThoughtById)
-// .put(updateThought)
-// .delete(deleteThought);
+// /api/thoughts/:id
+router.route('/:id')
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
 
 // // /api/thoughts/:thoughtId/reactions
 // router.route('/:id/reactions')
